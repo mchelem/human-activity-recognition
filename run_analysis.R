4b7628cf74c92b03053af3d4301bfe24ce8eb465
Michele Silva <michele.s@icanlocalize.com>
2014-05-25 19:58:36 -0300

    Combines the test and train data sets in a single data frame

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+
+read.activities <- function() {
+	activities = read.table("UCI HAR Dataset/activity_labels.txt", colClasses=c("numeric", "character"))
+	names(activities) <- c("id","activity")
+
+	# Improve activities' name readability
+	activities$activity <- tolower(activities$activity)
+	activities$activity <- sub("_", " ", activities$activity)
+	activities
+}
+
+
+read.features <- function() {	
+	features <- read.table("UCI HAR Dataset/features.txt", colClasses=c("numeric", "character"))
+	names(features) <- c("id", "name")
+
+	# Select only features of interest (mean and standard deviation)
+	features <- features[grepl("mean\\(|std\\(",features$name),]
+	# Remove spare parenthesis
+	features$name <- sub("()", "", features$name)
+	features
+}
+
+
+read.y <- function(type) {
+	y <- read.table(paste("UCI HAR Dataset/", type ,"/y_", type, ".txt", sep=""))
+	activities <- read.activities()
+	names(y) <- c("id")
+	y <- merge(y, activities, by="id")
+	y$id <- NULL
+	y
+}
+
+read.X <- function(type) {
+	X <- read.table(paste("UCI HAR Dataset/", type ,"/X_", type, ".txt", sep=""))
+	features <- read.features()
+	X <- X[, features$id]
+	names(X) <- features$name
+	X
+}
+
+
+message("Preparing data set. This may take a while...")
+train.dataset <- cbind(read.y("train"), read.X("train"))
+test.dataset <- cbind(read.y("test"), read.X("test"))
+dataset <- rbind(train.dataset, test.dataset)
+
+output.file <- "human-activity-recongnition-mean-std.txt"
+write.table(dataset, output.file)
+message(paste("Finished. Result saved to", output.file, "."))
